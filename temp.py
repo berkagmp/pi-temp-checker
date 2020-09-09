af277f8adee1ba1b100377cb9724e382e0a6e2f0
Derek <berkagmp@gmail.com>
2020-09-10 10:15:50 +1200

    Add MongoDB logic

--- temp.py
+++ temp.py
@@ -1,11 +1,14 @@
 import subprocess
 from datetime import datetime
 from mysql.connector import MySQLConnection
+import pymongo
+import pytz
+import datetime
 
 import config
 
 
-def insertDB(array):
+def insertMySQL(array):
   cpu = float(array[1])
   gpu = float(array[2][0:array[2].find("'")])
 
@@ -28,8 +31,29 @@ def insertDB(array):
       cursor.close()
 
 
+def insertMongoDB(array):
+    cpu = float(array[1])
+    gpu = float(array[2][0:array[2].find("'")])
+
+    timezone = pytz.timezone(config.timezone)
+    aware_datetime = timezone.localize(datetime.datetime.now())
+
+    print(aware_datetime.isoformat())
+
+    myclient = pymongo.MongoClient(config.mongo_client)
+    mydb = myclient[config.database]
+    mycol = mydb[config.collection]
+
+    data = {"cpu": cpu, "gpu": gpu,
+            "created_at": aware_datetime.replace(tzinfo=pytz.UTC)}
+
+    x = mycol.insert_one(data)
+
+
 temp = subprocess.check_output(['temp'])
 # temp = "2020-09-06 18:11:34/44.7/45.0'C"
 
 array = str(temp).split('/')
-insertDB(array)
+
+insertMySQL(array)
+insertMongoDB(array)
